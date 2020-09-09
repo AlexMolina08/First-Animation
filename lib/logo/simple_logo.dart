87595e7e46041737aaff5688e4da854c515d7742
alex <alexmolina@gmail.com>
2020-09-09 18:35:55 +0200

    Creado animationController y animatio

--- lib/logo/simple_logo.dart
+++ lib/logo/simple_logo.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class Logo extends StatelessWidget {
+class Logo extends StatefulWidget {
+
+  @override
+  _LogoState createState() => _LogoState();
+}
+
+class _LogoState extends State<Logo> with SingleTickerProviderStateMixin{
+  AnimationController animCtrl;
+  Animation anim ;
+
+  @override
+  void initState() {
+    super.initState();
+    animCtrl = AnimationController(
+      duration: Duration(seconds: 5), //Lo que dura la animacion en completarse
+      vsync: this
+    );
+
+
+    anim = Tween<double>(begin: 0 , end: 2 * pi)
+        .animate(animCtrl)
+        ..addListener(() {
+          setState(() {}); //Cada vez que el animated cambia , se llama a setState
+        })..addStatusListener((status) {
+          if(status == AnimationStatus.completed) //si la animacion termina
+            animCtrl.reverse(); //llamamos al metodo reverse (animCtrll va de 1 a 0)
+          else if (status == AnimationStatus.dismissed)
+            animCtrl.forward();
+      });
+
+    animCtrl.forward(); //comienza la animacion
+  }
+
   @override
   Widget build(BuildContext context) {
     return Transform.rotate(
-      angle: 3,
+      angle: anim.value,
       child: Container(
           padding: EdgeInsets.all(10.0),
           width: 300.0,
@@ -16,4 +49,11 @@ class Logo extends StatelessWidget {
           )),
     );
   }
+
+  @override
+  void dispose() {
+    animCtrl.dispose();
+    super.dispose();
+  }
+
 }
